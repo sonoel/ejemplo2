f23ca124683b5d3b9207a0b8e02f1076fd038706
sonoel <48770276+sonoel@users.noreply.github.com>
2019-05-14 15:22:57 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -165,7 +165,7 @@ function queryDetalleSuccess(tx, results) {
 		$("#foto_img").attr("src", _foto);
 		$("#nombre").html($.registro.nombre + " " + $.registro.apellidos);
 		$("#num_tel").html($.registro.telefono);
-		$("#telefono").attr("href", "tel:" + $.registro.telefono);
+		$("#telefono").attr("href", $.registro.telefono);
 		$("#label_mail").html("Mail: " + $.registro.email);
 }
 
